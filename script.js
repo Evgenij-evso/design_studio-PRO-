2603e1760ae6f8025bf3d6ed3c14b8a6757e4982
Ev.s.o <110043767+Evgenij-evso@users.noreply.github.com>
2023-11-17 02:34:12 +0300

    logo+

---
+++ script.js
@@ -0,0 +1,12 @@
+let penis_yes = document.querySelector('.YES_penis-container')
+let button_contact = document.querySelector('.button_contact')
+let penis_very_yes = document.querySelector('.penis_very_yes')
+
+button_contact.addEventListener('click',function(){
+    penis_yes.setAttribute('display', 'flex')
+    console.log('sdgsdgfs')
+})
+penis_very_yes.addEventListener('click',function(){
+    penis_yes.setAttribute('display', 'none')
+    console.log('sdgsdgfs')
+})
